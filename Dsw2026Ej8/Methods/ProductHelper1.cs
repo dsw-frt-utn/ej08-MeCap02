7001b728e3e80e78540d288cbabe9041912ba1ce
Mateo Costilla <mateocostilla123@gmail.com>
2026-05-01 12:09:24 -0300

    problema 2

--- Dsw2026Ej8/Methods/ProductHelper1.cs
+++ Dsw2026Ej8/Methods/ProductHelper1.cs
@@ -4,9 +4,15 @@ using System.Text;
 
 namespace Dsw2026Ej8.Methods
 {
-    internal partial class ProductHelper1
+    internal partial class ProductHelper
     {
         public static string ObtenerEtiquetaProducto(long code, string description, decimal price) => $"[{code}] {description} - {price}";
 
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            quantity = ValidateQuantity(quantity);
+            var myAnonimousObject = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = quantity * unitPrice };
+            return $"{myAnonimousObject.Code}-{myAnonimousObject.Description}-{myAnonimousObject.Total}";
+        }
     }
 }
--- Dsw2026Ej8/Methods/ProductHelper2.cs
+++ Dsw2026Ej8/Methods/ProductHelper2.cs
@@ -4,13 +4,22 @@ using System.Text;
 
 namespace Dsw2026Ej8.Methods
 {
-    internal class ProductHelper2
+    internal partial class ProductHelper
     {
         public static void WriteElements(string? code, string? description, decimal? price)
         {
             Console.WriteLine(code);
             Console.WriteLine(description);
             Console.WriteLine(price);
+        }
+
+        public static int ValidateQuantity(int quantity)
+        {
+            if (quantity <= 0)
+            {
+                quantity = 0;
+            }
+            return quantity;
         }
     }
 }
