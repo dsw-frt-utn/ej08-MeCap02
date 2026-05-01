592145a7bc6fc4a440fea9ab2c18dc67fdb5160a
Mateo Costilla <mateocostilla123@gmail.com>
2026-05-01 12:29:56 -0300

    problema 4

--- Dsw2026Ej8/Methods/ProductHelper1.cs
+++ Dsw2026Ej8/Methods/ProductHelper1.cs
@@ -24,5 +24,23 @@ namespace Dsw2026Ej8.Methods
             auxProduct.SetProductDescription("");
             return $"{originalValue}-{auxValue}-{auxProduct.GetProductDescription()}";
         }
+
+        public double CalcularPromedio(int? nota1 = null, int? nota2 = null, int? nota3 = null)
+        {
+            int? Verificar(int? aux)
+            {
+                if ((aux < 0) || (aux > 10))
+                {
+                    aux = 0;
+                }
+                return aux;
+            }
+
+            nota1 = Verificar(nota1);
+            nota2 = Verificar(nota2);
+            nota3 = Verificar(nota3);
+            
+            return (nota1 ?? 0 + nota2 ?? 0 + nota3 ?? 0) / 3;
+        }
     }
 }
