b4ae56fc67b662c9d234d1c73a8eded96b809de9
Mateo Costilla <mateocostilla123@gmail.com>
2026-05-01 17:04:20 -0300

    ejercicio 6

---
+++ Dsw2026Ej8/Methods/problema6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dsw2026Ej8.Models;
+
+namespace Dsw2026Ej8.Methods
+{
+    internal class problema6
+    {
+        public string NormalizarCodigoProducto(string code)
+        {
+            return code.ToProductCode();
+        }
+    }
+}
---
+++ Dsw2026Ej8/Models/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Models
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string? code)
+        {
+            string newCode = "";
+            if(code is null)
+            {
+                newCode = "SIN-CODIGO";
+            }
+            else
+            {
+                foreach (char aux in code.ToUpper())
+                {
+                    if (aux == ' ')
+                    {
+                        newCode += '-';
+                    }
+                    else
+                    {
+                        newCode += aux;
+                    }
+                }
+            }
+            return newCode;
+        }
+    }
+}
