b7a9dab853766bbecd13f3b6e7306b62d769ddbb
Mateo Costilla <mateocostilla123@gmail.com>
2026-05-01 12:05:10 -0300

    problema 1

---
+++ Dsw2026Ej8/Methods/ProductHelper1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Methods
+{
+    internal partial class ProductHelper1
+    {
+        public static string ObtenerEtiquetaProducto(long code, string description, decimal price) => $"[{code}] {description} - {price}";
+
+    }
+}
---
+++ Dsw2026Ej8/Methods/ProductHelper2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.Methods
+{
+    internal class ProductHelper2
+    {
+        public static void WriteElements(string? code, string? description, decimal? price)
+        {
+            Console.WriteLine(code);
+            Console.WriteLine(description);
+            Console.WriteLine(price);
+        }
+    }
+}
